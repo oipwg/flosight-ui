7ec226faa297ad615ecaf65f7473e1b0d5ab493e
Sky Young <sky@alexandria.io>
2018-05-22 11:21:57 -0700

    Use ClipboardJS for HTML5 copy

--- public/src/js/directives.js
+++ public/src/js/directives.js
@@ -40,36 +40,29 @@ angular.module('flosight')
       }
     };
   })
-  .directive('clipCopy', function() {
-    ZeroClipboard.config({
-      moviePath: '/lib/zeroclipboard/ZeroClipboard.swf',
-      trustedDomains: ['*'],
-      allowScriptAccess: 'always',
-      forceHandCursor: true
-    });
-
+  .directive('dataClipboardText', function() {
     return {
       restric: 'A',
-      scope: { clipCopy: '=clipCopy' },
+      scope: { dataClipboardText: '=dataClipboardText' },
       template: '<div class="tooltip fade right in"><div class="tooltip-arrow"></div><div class="tooltip-inner">Copied!</div></div>',
       link: function(scope, elm) {
-        var clip = new ZeroClipboard(elm);
+        var clip = new ClipboardJS(elm);
 
-        clip.on('load', function(client) {
-          var onMousedown = function(client) {
-            client.setText(scope.clipCopy);
-          };
+        // clip.on('load', function(client) {
+        //   var onMousedown = function(client) {
+        //     client.setText(scope.clipCopy);
+        //   };
 
-          client.on('mousedown', onMousedown);
+        //   client.on('mousedown', onMousedown);
 
-          scope.$on('$destroy', function() {
-            client.off('mousedown', onMousedown);
-          });
-        });
+        //   scope.$on('$destroy', function() {
+        //     client.off('mousedown', onMousedown);
+        //   });
+        // });
 
-        clip.on('noFlash wrongflash', function() {
-          return elm.remove();
-        });
+        // clip.on('noFlash wrongflash', function() {
+        //   return elm.remove();
+        // });
       }
     };
   })
